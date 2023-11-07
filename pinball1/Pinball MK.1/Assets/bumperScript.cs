b908896448da89caaa80fd83e66afd3066ad6eee
Tarkinz <132514231+Tarkinz@users.noreply.github.com>
2023-11-06 18:25:23 -0800

    1st push

---
+++ pinball1/Pinball MK.1/Assets/bumperScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bumperScript : MonoBehaviour
+{	public float restPosition = 0f;
+	public float pressedPosition = 45f;
+	public float hitStrength = 10000f;
+	public float bumperDamper = 150f;
+	HingeJoint hinge;
+	public string inputName;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+	hinge.useSpring = true;
+    }
+
+    void Update()
+    {
+	JointSpring spring = new JointSpring();
+	spring.spring = hitStrength;
+	spring.damper = bumperDamper;
+
+	if (Input.GetAxis(inputName) == 1)
+	{
+	spring.targetPosition = pressedPosition;}
+	else 
+	{spring.targetPosition = restPosition;}
+	hinge.spring = spring;
+	hinge.useLimits = true;
+        
+    }
+}
